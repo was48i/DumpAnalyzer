69b9ad686cb392e9d7b8410b0bed84583228bae9
was48i <folklore.hy@gmail.com>
2019-09-16 01:48:08 +0800

    add component tag - hyang

--- main.py
+++ main.py
@@ -4,30 +4,28 @@
 import os
 import re
 
-source_dir = "/Users/hyang/workspace/hana"
 
-
-def dfs_dir(path):
-    with open(path + "/CMakeLists.txt") as file:
-        component_pattern = re.compile(r"SET_COMPONENT\(.*?\)", re.S)
-        directory_pattern = re.compile(r"ADD_SUBDIRECTORY\(.*?\)", re.S)
-        dir_list = []
-        for directory in directory_pattern.findall(file.read()):
-            dir_list.append(re.split(r"[(\")]+", directory)[1])
-        print(dir_list)
-        path_dir = dict()
-        for component in component_pattern.findall(file.read()):
-            if component.split("\"")[2] == ")":
-                for node in dir_list:
-                    path_dir[path + "/" + node] = component.split("\"")[1]
-            else:
-                for node in component.split("\"")[2].split("\n"):
-                    if re.search(r"\w", node):
-                        path_dir[path + "/" + node] = component.split("\"")[1]
-        for node in dir_list:
-            child_node = path + "/" + node
-            dfs_dir(child_node)
+def dfs_repo(path):
+    if os.path.exists(os.path.join(path, "CMakeLists.txt")):
+        with open(os.path.join(path, "CMakeLists.txt")) as file:
+            com_pattern = re.compile(r"SET_COMPONENT\(\".*?\".*?\)", re.S)
+            com_list = com_pattern.findall(file.read())
+            if com_list:
+                for com in com_list:
+                    if com.split("\"")[2] == ")":
+                        for node in os.listdir(path):
+                            path_map[os.path.join(path, node)] = com.split("\"")[1]
+                    else:
+                        for node in com.split("\"")[2].split("\n"):
+                            if re.search(r"\w", node):
+                                path_map[os.path.join(path, node.strip())] = com.split("\"")[1]
+    for node in os.listdir(path):
+        child_node = os.path.join(path, node)
+        if os.path.isdir(child_node):
+            dfs_repo(child_node)
 
 
 if __name__ == '__main__':
-    dfs_dir(source_dir)
+    path_map = dict()
+    repository = "/Users/hyang/workspace/hana"
+    dfs_repo(repository)
