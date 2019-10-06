2d0f74f4e57bb4c1277cd07cf91a7f8a32d16f65
was48i <folklore.hy@gmail.com>
2019-10-06 20:09:52 +0800

    add fully_qualified - hyang

--- main.py
+++ main.py
@@ -5,69 +5,62 @@ import os
 import re
 import glob
 import argparse
+
 from clang.cindex import Index
 from clang.cindex import Config
 
 
-def find_component(path):
+def find_comp(path):
     # read file
-    with open(path, "r") as file:
-        file_text = file.read()
+    with open(path, "r") as f:
+        file_text = f.read()
     # set patterns
-    com_pattern_1 = re.compile(r"SET_COMPONENT\(\"(.+)?\"\)", re.M)
-    com_pattern_2 = re.compile(r"SET_COMPONENT\(\"(.+)?\"([^)]+)\)", re.M)
+    pattern_1 = re.compile(r"SET_COMPONENT\(\"(.+)?\"\)", re.M)
+    pattern_2 = re.compile(r"SET_COMPONENT\(\"(.+)?\"([^)]+)\)", re.M)
     # get matching lists
-    com_list_1 = com_pattern_1.findall(file_text)
-    com_list_2 = com_pattern_2.findall(file_text)
-    return com_list_1, com_list_2
+    list_1 = pattern_1.findall(file_text)
+    list_2 = pattern_2.findall(file_text)
+    return list_1, list_2
 
 
-def best_match(path):
+def fully_qualified(child, path):
+    if child.location.file is None:
+        return ""
+    elif child.location.file.name != path:
+        return ""
+    else:
+        res = fully_qualified(child.semantic_parent, path)
+        if res != "":
+            return res + "::" + child.spelling
+    return child.spelling
+
+
+def find_func(path):
+    res = []
+    index = Index.create()
+    extra_header = os.path.join(src_path, "rte", "rtebase", "include")
+    args_list = ["-x", "c++", "-I" + src_path, "-I" + extra_header]
+    tu = index.parse(path, args_list)
+    for child in tu.cursor.walk_preorder():
+        if child.location.file is not None and child.location.file.name == path:
+            text = child.spelling or child.displayname
+            if text:
+                kind = str(child.kind)[str(child.kind).index('.') + 1:]
+                if kind in ["FUNCTION_DECL", "CONSTRUCTOR"]:
+                    res.append(fully_qualified(child, path))
+    return res
+
+
+def best_matched(path):
     if path in path_map:
-        component = path_map[path]
+        comp = path_map[path]
     else:
         while True:
             path = path[:path.rindex("/")]
             if path in path_map:
-                component = path_map[path]
+                comp = path_map[path]
                 break
-    return component
-
-
-def merge_symbol(deep, text):
-    global prefix
-    global pre_deep
-    if deep == 1:
-        prefix = text
-        symbol_list.append(prefix)
-        pre_deep = deep
-    else:
-        if deep > pre_deep:
-            symbol_list[-1] = symbol_list[-1] + "::" + text
-            pre_deep = deep
-        else:
-            symbol_list.append(prefix + "::" + text)
-            pre_deep = deep
-
-
-def dump_node(node, deep):
-    for child in node.get_children():
-        if child.location.file is not None and child.location.file.name == node.displayname:
-            text = child.spelling or child.displayname
-            if text:
-                kind = str(child.kind)[str(child.kind).index('.') + 1:]
-                if kind in ["NAMESPACE", "FUNCTION_DECL", "CLASS_DECL", "CONSTRUCTOR"]:
-                    merge_symbol(deep, text)
-                    dump_node(child, deep + 1)
-    return symbol_list
-
-
-def find_symbol(path):
-    index = Index.create()
-    header = os.path.join(args.source, "rte", "rtebase", "include")
-    args_list = ["-x", "c++", "-I" + args.source, "-I" + header]
-    tu = index.parse(path, args_list)
-    return dump_node(tu.cursor, 1)
+    return comp
 
 
 def dfs_repo(path):
@@ -75,13 +68,13 @@ def dfs_repo(path):
     cur_path = os.path.join(path, "CMakeLists.txt")
     if os.path.exists(cur_path):
         # get 2 types of component
-        parent_list, child_list = find_component(cur_path)
+        parent_list, child_list = find_comp(cur_path)
         # save parent component
-        for com in parent_list:
-            path_map[path] = com
+        for comp in parent_list:
+            path_map[path] = comp
         # save child component
-        for com in child_list:
-            for node in com[1].split("\n"):
+        for comp in child_list:
+            for node in comp[1].split("\n"):
                 if node.strip():
                     # support Windows
                     node_path = path
@@ -89,39 +82,35 @@ def dfs_repo(path):
                         node_path = os.path.join(node_path, node_dir)
                     # support "*"
                     for wildcard in glob.iglob(node_path):
-                        path_map[wildcard] = com[0]
+                        path_map[wildcard] = comp[0]
     # DFS repository
     for node in os.listdir(path):
-        child_node = os.path.join(path, node)
-        extension = os.path.splitext(child_node)
-        if os.path.isdir(child_node):
-            dfs_repo(child_node)
+        child = os.path.join(path, node)
+        extension = os.path.splitext(child)
+        if os.path.isdir(child):
+            dfs_repo(child)
         elif extension[-1] in [".h", ".hpp"]:
-            pass
-            # if find_symbol(child_node):
-            #     for symbol in find_symbol(child_node):
-            #         symbol_map[symbol] = best_match(child_node)
+            if find_func(child):
+                print(find_func(child))
+                for func in set(find_func(child)):
+                    func_map[func] = best_matched(child)
 
 
 if __name__ == "__main__":
+    src_path = r"/hana"
+    path_map = dict()
+    func_map = dict()
     # load libclang.so
-    libclangPath = r"/usr/local/lib"
+    lib_path = r"/usr/local/lib"
     if Config.loaded:
         pass
     else:
-        Config.set_library_path(libclangPath)
+        Config.set_library_path(lib_path)
     # parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--source", dest="source", help="source code path")
+    parser.add_argument("-d1", "--dump1", dest="dump1", help="crash dump file 1")
+    parser.add_argument("-d2", "--dump2", dest="dump2", help="crash dump file 2")
     args = parser.parse_args()
 
-    repo_path = args.source
-    path_map = dict()
-    symbol_map = dict()
+    dfs_repo(src_path)
 
-    prefix = ""
-    pre_deep = 0
-    symbol_list = []
-
-    # dfs_repo(repo_path)
-    print(find_symbol(r"/Users/hyang/workspace/hana/ptime/query/sqlscript/util/planviz_scope.h"))
