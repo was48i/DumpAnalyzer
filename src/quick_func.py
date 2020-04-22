92beb883fd408ff5514c07b0baebf6d78ed03d56
was48i <folklore.hy@gmail.com>
2020-04-22 17:31:58 +0800

    apply trie tree to approach - hyang

---
+++ src/quick_func.py
@@ -0,0 +1,42 @@
+import os
+import json
+
+from multiprocessing import Pool
+from persistence import load_functions
+
+
+def trie_tree(func):
+    res = dict()
+    print(func)
+    key = func
+    item_list = func.split("::")
+    for i, item in enumerate(item_list):
+        component_set = set()
+        prefix = func[:func.index(item) + len(item)]
+        for name in func_dict:
+            if name.startswith(prefix) and name.split("::")[i] == item:
+                component_set.add(func_dict[name])
+                if len(component_set) > 1:
+                    break
+        if len(component_set) == 1:
+            key = prefix
+            break
+    res[key] = func_dict[func]
+    return res
+
+
+def quick_func():
+    quick_funs = dict()
+    pool = Pool(4)
+    results = pool.map(trie_tree, func_dict.keys())
+    for res in results:
+        for k in res.keys():
+            quick_funs[k] = res[k]
+    dump_path = os.path.join(os.getcwd(), "json", "quick_functions-master.json")
+    with open(dump_path, "w") as fp:
+        json.dump(quick_funs, fp, indent=4, sort_keys=True)
+
+
+if __name__ == "__main__":
+    func_dict = load_functions()
+    quick_func()
