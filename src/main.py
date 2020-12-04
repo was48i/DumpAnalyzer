62fd0446cf44b0057dbf175cad132b4cd0b6281f
was48i <folklore.hy@gmail.com>
2020-12-04 09:33:53 +0000

    update Workflow class - hyang

--- src/main.py
+++ src/main.py
@@ -4,26 +4,22 @@
 import argparse
 import component
 import detection
-import function
-import time
+import stop_source
+import utils
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--update", nargs="?", const=True, help="Update components/functions.")
-parser.add_argument("-t", "--train", nargs="?", const=True, help="Training for parameter tuning.")
-parser.add_argument("-c", "--compare", nargs=2, help="Compare original call stacks.")
-parser.add_argument("-d", "--detect", nargs=2, help="Detect crash dump similarity.")
+parser.add_argument("--update", nargs="?", const=True, help="Update components/functions.")
+parser.add_argument("--train", nargs="?", const=True, help="Training for parameter tuning.")
+parser.add_argument("--compare", nargs=2, help="Compare original call stacks.")
+parser.add_argument("--detect", nargs=2, help="Detect crash dump similarity.")
+parser.add_argument("--stop", nargs="?", const=True, help="Count file names that can be filtered.")
 args = parser.parse_args()
 
 if __name__ == "__main__":
     # update components/functions
     if args.update:
-        start = time.time()
-        cpnt = component.Component()
-        cpnt.update_component()
-        func = function.Function()
-        func.update_function()
-        end = time.time()
-        print(end - start)
+        kdetector = component.Component()
+        kdetector.update_component()
     # training for parameter tuning
     if args.train:
         pass
@@ -32,5 +28,9 @@ if __name__ == "__main__":
         pass
     # detect crash dump similarity
     if args.detect:
-        item = detection.Detection(args.detect)
-        item.temp()
+        kdetector = detection.Detection(args.detect)
+        kdetector.detect_sim()
+    # count filtered file names
+    if args.stop:
+        kdetector = stop_source.StopSource()
+        kdetector.count_source()
--- src/workflow.py
+++ src/workflow.py
@@ -1,226 +1,121 @@
+import configparser
+import math
 import os
+import pymongo
 import re
-import json
 import subprocess
-import numpy as np
-
-from lcs_dp import lcs_dp
-from argument import parser
-#from component import to_component
-
-args = parser.parse_args()
-# load stop words
-stop_words_path = os.path.join(os.getcwd(), "json", "stop_words.json")
-try:
-    with open(stop_words_path, "r") as f:
-        stop_words = json.load(f)
-except FileNotFoundError:
-    print("Can not find stop_words, please check.")
 
 
-def valid_function(func):
-    # demangle
-    arguments = ["c++filt", "-p"]
-    if func.startswith("_Z"):
-        arguments.extend([func])
-        pipe = subprocess.Popen(arguments, stdout=subprocess.PIPE,
-                                stdin=subprocess.PIPE)
-        stdout, stderr = pipe.communicate()
-        func = stdout.decode("utf-8")[:-1]
-    # handle anonymous namespace
-    if "::(anonymous namespace)" in func:
-        func = func.replace("::(anonymous namespace)", "")
-    if "(anonymous namespace)::" in func:
-        func = func.replace("(anonymous namespace)::", "")
-    # remove parameter variable
-    if "(" in func:
-        func = func[:func.index("(")]
-    # remove template
-    if "<" in func:
-        func = func[:func.index("<")]
-    # remove return type
-    if re.match(r"^[a-z]+[ ]", func):
-        func = func[func.index(" ") + 1:]
-    if not re.match(r"^[_a-zA-Z]", func):
-        return ""
-    else:
-        return func
+class Workflow(object):
+    """
+    The whole process of our approach that contains several stages.
+    """
+    config = configparser.ConfigParser()
+    config_path = os.path.join(os.getcwd(), "settings.ini")
+    config.read(config_path)
+    # Git
+    git_dir = config.get("git", "dir")
+    # MongoDB
+    host = config.get("mongodb", "host")
+    port = config.get("mongodb", "port")
+    db = config.get("mongodb", "db")
+    coll = config.get("mongodb", "coll_cpnt")
+    # Model
+    stop_source = [i.strip() for i in config.get("model", "stop").split(",")]
+    m = config.getfloat("model", "m")
+    n = config.getfloat("model", "n")
 
-
-def extract_backtrace(text):
-    bt = ""
-    bt_pattern = re.compile(r"-\n[ ]*\d+:[ ](.+)"
-                            r"([^-]+Source:[ ].+:)*", re.M)
-    bt_functions = bt_pattern.findall(text)
-    for func_tuple in bt_functions:
-        func_info = list(func_tuple)
-        # merge into a line
-        if " + 0x" in func_info[0]:
-            func_info[0] = func_info[0][:func_info[0].index(" + 0x")]
-        # get valid function name
-        name = valid_function(func_info[0])
-        if name == "":
-            continue
-        if func_info[1]:
-            func_info[1] = func_info[1][func_info[1].index("Source: ") + 8:-1]
-            if "/" in func_info[1]:
-                bt += name + " at " + func_info[1] + "\n"
-            else:
-                bt += name + "\n"
-        else:
-            bt += name + "\n"
-    return bt
-
-
-def extract_exception(text):
-    ex = ""
-    # get ex_functions
-    ex_pattern = re.compile(r"\d+:[ ](.+[+].+[ ]at[ ].+):", re.M)
-    ex_pattern_sp = re.compile(r"\d+:[ ](.+[+].+)[ ]", re.M)
-    ex_functions = ex_pattern.findall(text)
-    if not ex_functions:
-        ex_functions = ex_pattern_sp.findall(text)
-    for line in ex_functions:
-        # remove offset if exists
-        offset_pattern = re.compile(r"([ ]const)*[+]*0x\w+([ ]in[ ])*")
-        func_info = re.sub(offset_pattern, "", line)
-        # handle exception without source
-        source = ""
-        if " at " in func_info:
-            f_name = func_info[:func_info.index(" at ")]
-            source = func_info[func_info.index(" at ") + 4:]
-        else:
-            f_name = func_info
-        # get valid function name
-        name = valid_function(f_name)
-        if name == "":
-            continue
-        # combine function and source
-        if "/" in source:
-            ex += name + " at " + source + "\n"
-        else:
-            ex += name + "\n"
-    return ex
-
-
-def filter_word(stacks):
-    start = -1
-    end = len(stacks)
-    # clean source information
-    pure_stacks = []
-    for function in stacks:
-        if " at " in function:
-            function = function[:function.index(" at ")]
-            pure_stacks.append(function)
-        else:
-            pure_stacks.append(function)
-    # filter prefix/suffix words
-    prefix_words, suffix_words = stop_words
-    for word in prefix_words:
-        if word in pure_stacks:
-            point = pure_stacks.index(word)
-            if point > start:
-                start = point
-    for word in suffix_words:
-        if word in pure_stacks:
-            point = pure_stacks.index(word)
-            if point < end:
-                end = point
-    return stacks[start + 1:end]
-
-
-def pre_process(path):
-    result = ""
-    with open(path, "r", encoding="ISO-8859-1") as fp:
-        file_text = fp.read()
-    # call stack pattern
-    stack_pattern = re.compile(r"\n(\[CRASH_STACK\][\s\S]+)"
-                               r"\[CRASH_REGISTERS\]", re.M)
-    content = stack_pattern.findall(file_text)
-    if not content:
-        return ""
-    else:
+    @staticmethod
+    def pre_process(dump_path):
+        result = []
+        with open(dump_path, "r", encoding="ISO-8859-1") as fp:
+            dump = fp.read()
+        # call stack pattern
+        stack_pattern = re.compile(r"\n(\[CRASH_STACK][\s\S]+)\[CRASH_REGISTERS]", re.M)
+        content = stack_pattern.findall(dump)
         stack = content[0]
-    # extract exception firstly
-    ex_location = "exception throw location:"
-    if ex_location in stack:
-        first_ex = stack.split(ex_location)[1]
-        result = extract_exception(first_ex)
-    # need to filter bt by stop_words
-    if result == "":
-        bt_result = extract_backtrace(stack)
-        bt_list = [i for i in bt_result.split("\n") if i]
-        filtered_bt = filter_word(bt_list)
-        for line in filtered_bt:
-            result += line + "\n"
-    return result
+        # obtain bt_functions
+        bt_pattern = re.compile(r"-\n[ ]*\d+:[ ](.+)[^-]+Source:[ ](.+):", re.M)
+        bt_functions = bt_pattern.findall(stack)
+        for func_tuple in bt_functions:
+            function, path = list(func_tuple)
+            # remove offset
+            offset_pattern = re.compile(r"([ ]const)*[ ][+][ ]0x.+")
+            function = re.sub(offset_pattern, "", function)
+            result.append([function, path])
+        return result
 
+    def best_matched(self, path):
+        """
+        Obtain the best matched component from components collection.
 
-def add_knowledge(processed):
-    result = []
-    component = ""
-    func_content = ""
-    for func_info in [i for i in processed.split("\n") if i]:
-        if " at " in func_info:
-            name = func_info[:func_info.index(" at ")]
-            source = func_info[func_info.index(" at ") + 4:]
-            func = [name, source]
+        Args:
+            path: The path of current header file.
+
+        Returns:
+            The best matched component.
+        """
+        matched = "UNKNOWN"
+        client = pymongo.MongoClient(host=self.host, port=int(self.port))
+        collection = client[self.db][self.coll]
+        result = collection.find_one({"path": path})
+        if result:
+            matched = result["component"]
         else:
-            func = [func_info, ""]
-        if component != "" and to_component(func) != component:
-            result.append([component, func_content[:-1]])
-            component = to_component(func)
-            func_content = ""
-            func_content += func[0] + "\n"
-        else:
-            component = to_component(func)
-            func_content += func[0] + "\n"
-    result.append([component, func_content[:-1]])
-    return result
+            while "/" in path:
+                path = path[:path.rindex("/")]
+                result = collection.find_one({"path": path})
+                if result:
+                    matched = result["component"]
+                    break
+        return matched
 
-
-def edit_distance(src, tgt):
-    m = len(src)
-    n = len(tgt)
-    dp = [[i + j for j in range(n + 1)] for i in range(m + 1)]
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if src[i - 1] == tgt[j - 1]:
-                d = 0
+    def add_knowledge(self, processed):
+        result = []
+        component = ""
+        functions = []
+        for frame in processed:
+            # obtain complete path and source
+            if "/" in frame[1]:
+                path = self.git_dir + "/" + frame[1]
+                source = frame[1][frame[1].rindex("/")+1:]
             else:
-                d = 1
-            dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1,
-                           dp[i - 1][j - 1] + d)
-    sum = m + n
-    return (sum - dp[m][n]) / sum
+                arguments = ["find", self.git_dir, "-name", frame[1]]
+                pipe = subprocess.Popen(arguments, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+                stdout, stderr = pipe.communicate()
+                path = stdout.decode("utf-8")[:-1]
+                source = frame[1]
+            # obtain best matched component
+            if source in self.stop_source:
+                continue
+            else:
+                if "\n" in path:
+                    cur_component = "UNKNOWN"
+                else:
+                    cur_component = self.best_matched(path)
+            # demangle
+            if frame[0].startswith("_Z"):
+                arguments = ["c++filt", "-p", frame[0]]
+                pipe = subprocess.Popen(arguments, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+                stdout, stderr = pipe.communicate()
+                frame[0] = stdout.decode("utf-8")[:-1]
+            # merge several functions into a component
+            if component == "" or cur_component == component:
+                component = cur_component
+                functions.append(frame[0])
+            else:
+                result.append([component, functions])
+                component = cur_component
+                functions = [frame[0]]
+        result.append([component, functions])
+        return result
 
-
-def calculate_sim(paths, m, n):
-    sim = 0.0
-    # prepare items for calculation
-    cpnt_list = []
-    func_list = []
-    for path in paths:
-        info = add_knowledge(pre_process(path))
-        cpnt_list.append([i[0] for i in info])
-        func_list.append([i[1] for i in info])
-    # calculate similarity
-    above_sum = 0.0
-    below_sum = 0.0
-    # apply LCS
-    lcs, x_pos, y_pos = lcs_dp(cpnt_list)
-    above_item = []
-    for i, com in enumerate(lcs):
-        pos = max(x_pos[i], y_pos[i])
-        # apply edit distance by element
-        src_list = re.split("\n|::", func_list[0][x_pos[i]])
-        tgt_list = re.split("\n|::", func_list[1][y_pos[i]])
-        component_sim = edit_distance(src_list, tgt_list)
-        # used for output
-        above_item.append([pos, component_sim])
-        above_sum += np.exp(-m * pos) * np.exp(-n * (1 - component_sim))
-    below_item = max(len(cpnt_list[0]), len(cpnt_list[1]))
-    for i in range(below_item):
-        below_sum += np.exp(-m * i)
-    sim = above_sum / below_sum
-    return sim, above_item, below_item
+    def calculate_sim(self, positions, distances, len_max):
+        numerator = 0.0
+        denominator = 0.0
+        for pos, dist in zip(positions, distances):
+            numerator += math.exp(-self.m * pos * -self.n * dist)
+        for i in range(len_max):
+            denominator += math.exp(-self.m * i)
+        similarity = numerator / denominator
+        return similarity
