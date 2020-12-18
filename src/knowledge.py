7836dcdf41f577eff98014a064fe1ad373d123bf
was48i <folklore.hy@gmail.com>
2020-12-18 10:06:32 +0000

    handle anonymous namespace - hyang

--- src/knowledge.py
+++ src/knowledge.py
@@ -7,6 +7,11 @@ from component import Component
 
 
 class Knowledge(object):
+    """
+    Add component knowledge and obtain cpnt_order, func_block for calculation.
+    Attributes:
+        processed: Processed crash dump composed of function and path.
+    """
     config = configparser.ConfigParser()
     config_path = os.path.join(os.getcwd(), "config.ini")
     config.read(config_path)
@@ -23,68 +28,80 @@ class Knowledge(object):
     def __init__(self, processed):
         self.processed = processed
 
-    def process_frame(self, frame):
-        function, path = frame
-        word = path[path.rindex("/") + 1:] if "/" in path else path
-        if word not in self.stop_words:
-            component = Component().to_component(path)
-            # demangle
-            if function.startswith("_Z"):
-                arguments = ["c++filt", "-p", function]
-                pipe = subprocess.Popen(arguments, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-                stdout, _ = pipe.communicate()
-                function = stdout.decode("utf-8")[:-1]
-            return component, function
-        else:
-            return "", ""
-
     @staticmethod
     def unboxing(function):
+        """
+        Extract the required part of function and divide it into blocks.
+        Args:
+            function: A original function name.
+        Returns:
+            Function blocks composed of class, namespace, ...
+        """
         blocks = []
+        # handle anonymous namespace
+        if "(anonymous namespace)::" in function:
+            function = function.replace("(anonymous namespace)::", "")
+        if "::(anonymous namespace)" in function:
+            function = function.replace("::(anonymous namespace)", "")
         if "(" in function:
             function = function[:function.index("(")]
         if "<" in function:
             function = function[:function.index("<")]
         if re.match(r"^[a-z]+[ ]", function):
             function = function[function.index(" ") + 1:]
+        # remove special characters
+        if re.search(r"[^0-9a-zA-Z:_~]", function):
+            point = re.search(r"[^0-9a-zA-Z:_~]", function).span()[0]
+            function = function[:point]
         for block in function.split("::"):
             blocks.append(block)
         return blocks
 
+    def process_frame(self, frame):
+        """
+        Process stack frame and obtain component and function information.
+        Args:
+            frame: A original stack frame.
+        Returns:
+            A component with function information which the current frame belongs to.
+        """
+        function, path = frame
+        word = path[path.rindex("/") + 1:] if "/" in path else path
+        if word in self.stop_words:
+            return "", ""
+        else:
+            component = Component().to_component(path)
+            if component == "UNKNOWN":
+                return "", ""
+            # demangle
+            if function.startswith("_Z"):
+                arguments = ["c++filt", "-p", function]
+                pipe = subprocess.Popen(arguments, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+                stdout, _ = pipe.communicate()
+                function = stdout.decode("utf-8")[:-1]
+            function = self.unboxing(function)
+            return component, function
+
     def add_knowledge(self):
+        """
+        Add component knowledge and obtain cpnt_order, func_block for calculation.
+        Returns:
+            The cpnt_order and func_block for calculation.
+        """
         cpnt_order = []
         func_block = []
         for frame in self.processed:
             component, function = self.process_frame(frame)
-            if not component:
+            if component == "":
                 continue
             # obtain cpnt_order and func_block
-            if cpnt_order:
+            if not cpnt_order:
+                cpnt_order = [component]
+                func_block = [function]
+            else:
                 if component != cpnt_order[-1]:
                     cpnt_order.append(component)
-                    func_block.append(self.unboxing(function))
+                    func_block.append(function)
                 else:
-                    func_block[-1].extend(self.unboxing(function))
-            else:
-                cpnt_order = [component]
-                func_block = [self.unboxing(function)]
+                    func_block[-1].extend(function)
         return cpnt_order, func_block
-
-    def merge_function(self):
-        result = []
-        component = ""
-        functions = []
-        for frame in self.processed:
-            cur_component, function = self.process_frame(frame)
-            if not cur_component:
-                continue
-            # merge several functions into a component
-            if component != "" and cur_component != component:
-                result.append([component, functions])
-                component = cur_component
-                functions = [function]
-            else:
-                component = cur_component
-                functions.append(function)
-        result.append([component, functions])
-        return result
