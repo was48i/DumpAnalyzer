6f2700ac29dc341e4b2ce39f8aa6e4b6e6c6361e
was48i <folklore.hy@gmail.com>
2020-12-18 10:09:24 +0000

    use textwrap to print crash dump - hyang

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 import configparser
 import difflib
 import os
+import textwrap
 
 
 class Logger(object):
@@ -16,29 +17,40 @@ class Logger(object):
     # Log
     width = config.getint("log", "width")
 
-    def dump_print(self, message, cursor_up):
+    def dump_print(self, message):
+        """
+        Print a pair of crash dumps in comparison way.
+        Args:
+            message: Crash dump message to be printed.
+        """
         print("\n", end="")
         # print the left part
-        for msg in message[0]:
-            component, functions = msg
-            print("\x1b[0;36m" + component + "\x1b[0m")
-            for func in functions:
-                print(func[:self.width])
-        # print the right part
+        cursor_up = 0
+        for index in range(len(message[0])):
+            blocks = textwrap.fill(" ".join(message[1][index]), width=self.width)
+            cursor_up += len(blocks.split("\n")) + 1
+            print("\x1b[0;36m{}\x1b[0m".format(message[0][index]))
+            print(blocks)
         print("\x1b[{}A".format(cursor_up), end="")
-        for msg in message[1]:
-            component, functions = msg
-            if component != "":
-                print("\x1b[{}C".format(self.width + 2), end="")
-                print("|  " + "\x1b[0;36m" + component + "\x1b[0m")
-                for func in functions:
-                    print("\x1b[{}C".format(self.width + 2), end="")
-                    print("|  " + func[:self.width])
-            else:
-                print("\x1b[{}C".format(self.width + 2), end="")
-                print("|  ")
+        # print the right part
+        cursor_down = 0
+        for index in range(len(message[2])):
+            blocks = textwrap.fill(" ".join(message[3][index]), width=self.width)
+            cursor_down += len(blocks.split("\n")) + 1
+            print("\x1b[{}C  |  \x1b[0;36m{}\x1b[0m".format(self.width, message[2][index]))
+            for line in blocks.split("\n"):
+                print("\x1b[{}C  |  {}".format(self.width, line))
+        if cursor_down < cursor_up:
+            print("\x1b[{}B".format(cursor_up - cursor_down), end="")
 
     def formula_print(self, features, len_max, sim):
+        """
+        Print the formula of similarity calculation.
+        Args:
+            features: Feature values of position and distance.
+            len_max: The longer length of 2 component sequences.
+            sim: Similarity result.
+        """
         print("\n", end="")
         numerator = ""
         denominator = ""
@@ -56,9 +68,16 @@ class Logger(object):
             print("             " + denominator)
         else:
             print("Similarity = 0.00%")
+        print("\n", end="")
 
     @staticmethod
     def diff_print(message, paths):
+        """
+        Print crash dump comparison by combined diff format.
+        Args:
+            message: Crash dump message to be printed.
+            paths: The paths of original crash dump pair.
+        """
         print("\n", end="")
         diff = difflib.unified_diff(message[0].split("\n"), message[1].split("\n"),
                                     fromfile=os.path.basename(paths[0]), tofile=os.path.basename(paths[1]),
@@ -78,3 +97,4 @@ class Logger(object):
         if message[0] == message[1]:
             for line in message[0].split("\n"):
                 print(line)
+        print("\n", end="")
