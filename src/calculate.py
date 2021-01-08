a97a0e56e33e5cbebce12ebcccac04a93a2fe952
was48i <folklore.hy@gmail.com>
2021-01-08 09:18:20 +0000

    move obtain_feature to calculate - hyang

--- src/calculate.py
+++ src/calculate.py
@@ -2,13 +2,15 @@ import configparser
 import math
 import os
 
+from utils import DP
+
 
 class Calculate(object):
     """
     Calculate crash dump similarity through the mathematical model.
     Attributes:
-        features: Feature values of position and distance.
-        len_max: The longer length of 2 component sequences.
+        order_pair: The component order information within crash dump pair.
+        block_pair: The function block information within crash dump pair.
     """
     config = configparser.ConfigParser()
     config_path = os.path.join(os.getcwd(), "config.ini")
@@ -17,9 +19,22 @@ class Calculate(object):
     m = config.getfloat("model", "m")
     n = config.getfloat("model", "n")
 
-    def __init__(self, features, length):
-        self.features = features
-        self.len_max = length
+    def __init__(self, order_pair, block_pair):
+        self.order_pair = order_pair
+        self.block_pair = block_pair
+
+    def obtain_feature(self):
+        """
+        Obtain the features used for calculation through dump pair information.
+        Returns:
+            The features used for calculation.
+        """
+        positions, distances = [], []
+        pos_x, pos_y = DP().lcs_position(self.order_pair[0], self.order_pair[1])
+        for i, j in zip(pos_x, pos_y):
+            positions.append(max(i, j))
+            distances.append(DP().normalized_dist(self.block_pair[0][i], self.block_pair[1][j]))
+        return list(zip(positions, distances))
 
     def calculate_sim(self, m=m, n=n):
         """
@@ -32,9 +47,9 @@ class Calculate(object):
         """
         numerator = 0.0
         denominator = 0.0
-        for pos, dist in self.features:
+        for pos, dist in self.obtain_feature():
             numerator += math.exp(-m * pos) * math.exp(-n * dist)
-        for i in range(self.len_max):
+        for i in range(len(max(self.order_pair, key=len))):
             denominator += math.exp(-m * i)
         sim = numerator / denominator
         return sim
--- src/train.py
+++ src/train.py
@@ -6,12 +6,13 @@ from numpy import arange, array
 from pool import MongoConnection
 from sample import Sample
 from sklearn.metrics import roc_curve, auc
-from utils import DP
 
 
 class Train(object):
     """
     Training for parameter tuning which contains data sampling.
+    Attributes:
+        dataset: The sampled dataset.
     """
     config = configparser.ConfigParser()
     config_path = os.path.join(os.getcwd(), "config.ini")
@@ -22,25 +23,8 @@ class Train(object):
     db = config.get("mongodb", "db")
     coll = config.get("mongodb", "coll_data")
 
-    @staticmethod
-    def obtain_feature(order_pair, block_pair):
-        """
-        Obtain the features used for calculation through dump pair information.
-        Args:
-            order_pair: The component order information within crash dump pair.
-            block_pair: The function block information within crash dump pair.
-        Returns:
-            The features used for calculation.
-        """
-        positions = []
-        distances = []
-        order_x, order_y = order_pair
-        # top first
-        pos_x, pos_y = DP().lcs_position(order_x[::-1], order_y[::-1])
-        for i, j in zip(pos_x, pos_y):
-            positions.append(max(i, j))
-            distances.append(DP().normalized_dist(block_pair[0][i], block_pair[1][j]))
-        return list(zip(positions, distances))
+    def __init__(self):
+        self.dataset = Sample().sample_data()
 
     def predict_score(self, sample, m, n):
         """
@@ -58,24 +42,21 @@ class Train(object):
             target = collection.find_one({"test_id": sample[1]})
             order_pair = [source["cpnt_order"], target["cpnt_order"]]
             block_pair = [source["func_block"], target["func_block"]]
-        features = self.obtain_feature(order_pair, block_pair)
-        len_max = len(max(order_pair, key=len))
-        score = Calculate(features, len_max).calculate_sim(m, n)
+        score = Calculate(order_pair, block_pair).calculate_sim(m, n)
         return score
 
-    def draw_curve(self, dataset, m, n):
+    def draw_curve(self, m, n):
         """
         Obtain basic information for curve drawing.
         Args:
-            dataset: The sampled dataset.
             m: The parameter for component position.
             n: The parameter for component distance.
         Returns:
-            The basic information such as fpr, tpr, threshold...
+            The basic information such as fpr, tpr, threshold, ...
         """
         true_label = []
         pred_score = []
-        for label, samples in enumerate(dataset):
+        for label, samples in enumerate(self.dataset):
             for sample in samples:
                 score = self.predict_score(sample, m, n)
                 true_label.append(label)
@@ -84,24 +65,22 @@ class Train(object):
         pred_score = array(pred_score)
         return roc_curve(true_label, pred_score)
 
-    def debugging(self, dataset):
+    def debugging(self):
         """
         Output debugging information of training.
-        Args:
-            dataset: The sampled dataset.
         """
         print("\n", end="")
         threshold = 0.0000
         # obtain optimized parameters
         m = self.config.getfloat("model", "m")
         n = self.config.getfloat("model", "n")
-        fpr_list, _, threshold_list = self.draw_curve(dataset, m, n)
+        fpr_list, _, threshold_list = self.draw_curve(m, n)
         for i, fpr in enumerate(fpr_list):
             if fpr > 0:
                 threshold = threshold_list[i]
                 print("Threshold={:.2%}\n".format(threshold))
                 break
-        for label, samples in enumerate(dataset):
+        for label, samples in enumerate(self.dataset):
             for sample in samples:
                 score = self.predict_score(sample, m, n)
                 if label == 0 and score >= threshold:
@@ -117,12 +96,10 @@ class Train(object):
         m_opt = 0.0
         n_opt = 0.0
         auc_max = 0.0
-        # data sampling
-        dataset = Sample().sample_data()
         print("Start parameter tuning...\n")
-        for m in arange(0.0, 1.1, 0.1):
-            for n in arange(2.0, 3.1, 0.1):
-                fpr, tpr, _ = self.draw_curve(dataset, m, n)
+        for m in arange(1.0, 3.1, 0.1):
+            for n in arange(1.0, 3.1, 0.1):
+                fpr, tpr, _ = self.draw_curve(m, n)
                 roc_auc = auc(fpr, tpr)
                 print("m=%.1f, n=%.1f, AUC=%.3f" % (m, n, roc_auc))
                 if roc_auc > auc_max:
@@ -130,8 +107,8 @@ class Train(object):
                     n_opt = n
                     auc_max = roc_auc
         print("\nm_opt=%.1f, n_opt=%.1f, AUC_MAX=%.3f" % (m_opt, n_opt, auc_max))
-        # overwrite model parameters
+        # update model parameters
         self.config.set("model", "m", "%.1f" % m_opt)
         self.config.set("model", "n", "%.1f" % n_opt)
         self.config.write(open(self.config_path, "w"))
-        self.debugging(dataset)
+        self.debugging()
